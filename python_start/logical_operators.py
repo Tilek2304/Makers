b5f32f0a7871d4757d3fd8368fee7e5cfbfe1532
Tilek2304 <esenaliev2304@gmail.com>
2022-07-22 19:55:30 +0600

    modified logical_operators.py

---
+++ python_start/logical_operators.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+string = 'glhf'
+
+num = 10
+num2 = 20
+num < num2      # num1 меньше num2?
+num > num2      # num1 больше num2?
+num == num2     # num1 равен num2?
+num != num2     # num1 неравен num2?
+num <= num2     # num1 меньше или равен num2?
+num >= num2     # num1 больше или равен num2?
+num = ord('g')  # возвращает значение в таблице ascii
+symbol = chr(43)# возвращает символ в таблице ascii
+sorted(string)  # сортирует строку по символам согласно ascii
